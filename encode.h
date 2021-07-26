9128e6782644756ae1b99daac16aca9bfa8333ae
Owen Leishman <87945544+u-farad@users.noreply.github.com>
2021-07-26 12:33:22 -0600

    Add files via upload

---
+++ encode.h
@@ -0,0 +1,7 @@
+#ifndef ENCODE_H_
+#define ENCODE_H_
+
+#endif
+
+void disp(uint32_t output[6]);
+void encode(uint16_t input[16]);
---
+++ tlc5940.c
@@ -0,0 +1,36 @@
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "serial.pio.h"
+#include "coast.pio.h"
+#include "encode.h"
+#include <stdio.h>
+
+
+
+// pin connections
+// 1 - SIN
+// 2 - SCLK
+// 3 - BLANK
+// 4 - XLAT
+// 5 - GSCLK
+
+
+
+int main(){
+
+    gpio_init(9);
+    gpio_set_dir(9, 1);
+    gpio_put(9, 1);
+
+
+    // example data to output, must be 12 bit, FFF is max pulse width
+    int16_t test[16] ={0x0, 0xFFF, 0x0, 0x0, 0xFFF, 0x0, 0x0, 0xFFF, 0x0, 0x0, 0xFFF, 0x0, 0x0, 0xFFF, 0x0, 0x0};
+
+    // transmits data after encoding it, as well as setting up the state machines to run the tlc5940 without any cpu oversight
+    encode(test);
+}
+
+
+
+
+
